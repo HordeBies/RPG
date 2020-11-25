04e614b14fe31df9ec70de3f24a4c0e0f7142fb5
HordeBies <oa.mehmetdmrc@gmail.com>
2020-11-25 14:29:11 +0300

    comments updated

--- main.go
+++ main.go
@@ -12,9 +12,19 @@ func main() {
 }
 
 //game currently starts in Main Menu
-// Play button leads user to Construction Menu(wip)
+// Play button leads user to Selection Menu
+// Start button leads user to edit Menu
+// IN EDIT MODE
 //tools : 1 for dirt floor, 2 stone wall, 3 door, 4 mainCharacter(uses a placeholder texture)
+// door and maincharacter currently unavailable
 // ps. textures are created randomly within their scope
 //left click place(if possible such as wall and floor uses same layer but door is 1 layer above)
 //right click remove most upper layer
-// "s" save
+// "s" hard save
+
+//todo list
+//will implement entities above background
+//code beauty(there may be unused/unnecessary parts left while changing whole structure)
+//auto maze builder within scope
+//left right up down buttons for travelling on level(currently left upmost part is shown automaticly)
+//...
