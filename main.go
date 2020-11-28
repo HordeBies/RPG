93d8d87851d5fcc649797a825d6c25dfa347ed0f
Hakkı ÜLKÜ <hakki264ulku@gmail.com>
2020-11-28 21:06:21 +0300

    init

--- main.go
+++ main.go
@@ -36,4 +36,4 @@ func main() {
 //auto maze builder within scope
 //left right up down buttons for travelling on level(currently left upmost part is shown automaticly)
 // entity collision detection in build menu ie. doors cant overlap & there cant be more than 1 main character
-//...
+// ~H~
